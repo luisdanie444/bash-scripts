0d1f2e0f7464f158aa65fd20194980dc4e34a93c
Luis Torrealba <danielrosario445.t@gmail.com>
2026-02-12 17:38:14 -0400

    finishing my system report script

--- 01-basics/system-report.sh
+++ 01-basics/system-report.sh
@@ -3,7 +3,21 @@
 user=$(whoami)
 now=$(date)
 memory=$(free -h | grep -i mem)
-space=$(df -h | grep "dev")
+harddrive=$(df -h | grep / )
 
 #Displaying the user and current time
 echo "Welcome $user, Today's date is $now"
+echo ""
+
+#Showing the amount of system RAM
+echo "---SYSTEM RAM---"
+echo ""
+echo "$memory"
+echo ""
+
+#Showing storage space
+echo "---DISK SPACE---"
+echo ""
+echo "$harddrive"
+
+
